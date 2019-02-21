30b93ad576f7f44af76d3ae0348f6f645ec10549
Richard Parayno <broodheart@gmail.com>
2019-02-21 21:40:03 +0800

    initial styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import '../css/header.css'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="header-container">
+    <div>
+      <div className="header-links">
+        <a className="header-item">About Us</a>
+        <a className="header-item">About PMC</a>
+        <a className="header-item">The Organizers</a>
+        <a className="header-item">Contact Us</a>
+        <a className="header-item">Our Sponsors</a>
+      </div>
     </div>
   </header>
 )
