e4a6d180a64a458bd96890fd8090cb0f76be6f73
Richard Parayno <broodheart@gmail.com>
2019-03-03 20:38:19 +0800

    added class

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,20 @@ import React from "react"
 import '../css/header.css'
 
 const Header = ({ siteTitle }) => (
-  <header className="header-links">
-    <Link className="header-item pull-left" activeClassName="active" >Logo</Link>
-    <Link className="header-item" activeClassName="active" to='/'>Home</Link>
-    <Link className="header-item" activeClassName="active" to='/how-to-register'>How to Register</Link>
-    <Link className="header-item" activeClassName="active" to='/about-us'>About Us</Link>
-    <Link className="header-item" activeClassName="active" to='/about-pmc'>About PMC</Link>
-    {/* <Link className="header-item" activeClassName="active" to='/'>The Organizers</Link>
-    <Link className="header-item" activeClassName="active" to='/'>Contact Us</Link>
+  <header>
+    <div className="header-links">
+      <Link className="header-item pull-left" activeClassName="active" >Logo</Link>
+      <Link className="header-item" activeClassName="active" to='/'>Home</Link>
+      <Link className="header-item" activeClassName="active" to='/how-to-register'>How to Register</Link>
+      <Link className="header-item" activeClassName="active" to='/about-us'>About Us</Link>
+      <Link className="header-item" activeClassName="active" to='/about-pmc'>About PMC</Link>
+      {/* <Link className="header-item" activeClassName="active" to='/'>The Organizers</Link>
+      <Link className="header-item" activeClassName="active" to='/'>Contact Us</Link>
     <Link className="header-item" activeClassName="active" to='/'>Our Sponsors</Link> */}
+    </div>
+    <div className="header-links-mobile">
+      <p>Logo</p>
+    </div>
   </header>
 )
 
